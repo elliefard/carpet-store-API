904db4916d863bb07e1fca73d44fa9a78727da47
ellie fard <enesarifard@gmail.com>
2020-08-06 14:18:31 -0400

    added reset functionality, updating user, security

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const itemController = require('./../controllers/itemController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
 router
   .route('/')
-  .get(itemController.getAllItems)
+  .get(authController.protect, itemController.getAllItems)
   .post(itemController.createItem);
 router
   .route('/:id')
   .get(itemController.getItem)
   .patch(itemController.updateItem)
-  .delete(itemController.deleteItem);
+  .delete(authController.protect, authController.restrictTo('admin'), itemController.deleteItem);
 
 module.exports = router;
